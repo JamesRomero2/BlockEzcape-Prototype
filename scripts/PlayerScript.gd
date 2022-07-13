94cacfd0b6c564fcebf007cd614d604b215e5c8b
unknown <haymeemotoh22@gmail.com>
2022-07-13 11:33:32 +0800

    New Player Script

--- scripts/PlayerScript.gd
+++ scripts/PlayerScript.gd
@@ -1,38 +1,37 @@
 extends KinematicBody2D
 
-onready var collisionRay := $RayCast2D
-
-var gridSize := 16.00
-var canMove := true
-var playerInputDirection := Vector2.ZERO
+onready var verticalCollision = $Vectical
+onready var horizontalCollision = $Horizontal
 
 var inventoryResource = load("res://scripts/Inventory.gd")
 var inventory = inventoryResource.new()
 
+export(int) var speed := 0
+export(bool) var canMove := true
+var velocity := Vector2.ZERO
+
 func _physics_process(_delta):
 	if canMove:
 		if Input.is_action_just_pressed("ui_up"):
-			playerInputDirection = Vector2.UP
-			canMove = false
+			velocity = Vector2.UP
 		elif Input.is_action_just_pressed("ui_down"):
-			playerInputDirection = Vector2.DOWN
-			canMove = false
+			velocity = Vector2.DOWN
 		if Input.is_action_just_pressed("ui_left"):
-			playerInputDirection = Vector2.LEFT
-			canMove = false
+			velocity = Vector2.LEFT
 		elif Input.is_action_just_pressed("ui_right"):
-			playerInputDirection = Vector2.RIGHT
-			canMove = false
-	else:
-		playerMove(playerInputDirection)
+			velocity = Vector2.RIGHT
+		
+	if velocity != Vector2.ZERO:
+		canMove = false
 
-func playerMove(characterMoveDirection):
-	var movePosition = characterMoveDirection * gridSize
-	collisionRay.cast_to = movePosition / 2
-	collisionRay.force_raycast_update()
+	if velocity.x != 0:
+		horizontalCollision.disabled = false
+		verticalCollision.disabled = true
+	elif velocity.y != 0:
+		horizontalCollision.disabled = true
+		verticalCollision.disabled = false
+	
+	var collision = move_and_collide(velocity * speed)
 
-	if collisionRay.is_colliding():
+	if collision != null:
 		canMove = true
-		return
-	position += movePosition
-
